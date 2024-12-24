1d2f9d50318bf279c34f596c1c4c74787107f963
HarshaKhande <hkhande@gmail.com>
2024-12-24 12:48:40 +0530

    Modified Assembly file

--- AssemblyInfo1.cs
+++ AssemblyInfo1.cs
@@ -1,5 +1,6 @@
 using System.Runtime.InteropServices;
 using NUnit.Framework;
+using NUnit.Compatibility;
 
 // In SDK-style projects such as this one, several assembly attributes that were historically
 // defined in this file are now automatically added during build and populated with
